3e6796c4b5d3fce4ba74372f015adbe7e0a40627
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-01-18 07:22:43 +0100

    Add more non-stop stations

--- src/index.js
+++ src/index.js
@@ -4,8 +4,22 @@ import Nav from "./Nav"
 import Sheet from "./Sheet"
 
 const location = {
-  c: ["Äs", "Åbe", "Sst", "Sci", "Sod"],
-  n: ["So", "Udl", "Hel", "Sol", "Hgv", "Nvk", "R", "Upv", "Arnc"],
+  c: ["Äs", "Åbe", "Sst", "Sci", "Sod", "Tmö"],
+  n: [
+    "So",
+    "Kmy",
+    "Udl",
+    "Hel",
+    "Sol",
+    "Hgv",
+    "Nvk",
+    "R",
+    "Upv",
+    "Skby",
+    "Rs",
+    "Bra",
+    "Mr",
+  ],
   s: ["Rön", "Gau", "Södy", "Tu", "Uts", "Tul", "Flb", "Hu", "Sta"],
   e: [
     "Hfa",
@@ -20,7 +34,7 @@ const location = {
     "Fas",
     "Hön",
   ],
-  w: ["Sub", "Spå", "Bkb", "Jkb"],
+  w: ["Huv", "Sub", "Duo", "Spå", "Bkb", "Jkb"],
 }
 
 const state = { announcements: [], modified: "result.INFO" }
