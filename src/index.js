23d0108458c47535a11af5e03b88df1655de7267
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-01-23 20:26:41 +0100

    Move location lists to separate file

--- src/index.js
+++ src/index.js
@@ -1,40 +1,7 @@
 import { h, app } from "hyperapp"
 import Nav from "./Nav"
 import Sheet from "./Sheet"
-
-const location = {
-  c: ["Äs", "Åbe", "Sst", "Sci", "Sod", "Tmö"],
-  n: [
-    "So",
-    "Kmy",
-    "Udl",
-    "Hel",
-    "Sol",
-    "Hgv",
-    "Nvk",
-    "R",
-    "Upv",
-    "Skby",
-    "Rs",
-    "Bra",
-    "Mr",
-  ],
-  s: ["Rön", "Gau", "Södy", "Tu", "Uts", "Tul", "Flb", "Hu", "Sta"],
-  e: [
-    "Hfa",
-    "Ts",
-    "Kda",
-    "Vhe",
-    "Jbo",
-    "Hnd",
-    "Vga",
-    "Skg",
-    "Tåd",
-    "Fas",
-    "Hön",
-  ],
-  w: ["Huv", "Sub", "Duo", "Spå", "Bkb", "Jkb"],
-}
+import location from "./location"
 
 const state = { announcements: [], modified: "result.INFO" }
 
